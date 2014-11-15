f6ce2bc7202348a1fdb89f473e7b2e9cd6e0e67c
Charles J Hardy <chuckjhardy@gmail.com>
2014-11-15 17:36:27 +0000

    Add OmniAuth User Create Service

---
+++ spec/services/create_omniauth_user_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+describe CreateOmniauthUser do
+  subject { described_class.call user: user, auth: auth }
+
+  let(:user) { User.new }
+  let(:auth) do
+    {
+      uid: "123456",
+      info: {
+        nickname: "johnqpublic",
+        name: "John Q Public",
+        location: "Anytown, USA",
+      },
+      credentials: {
+        token: "a1b2c3d4",
+        secret: "abcdef1234"
+      },
+      extra: {
+        raw_info: {
+          lang: "en",
+          time_zone: "Chicago"
+        }
+      }
+    }
+  end
+
+  let(:user_details) do
+    {
+      "id" => nil,
+      "uid" => "123456",
+      "role" => 0,
+      "token" => "a1b2c3d4",
+      "secret" => "abcdef1234",
+      "name" => "John Q Public",
+      "nickname" => "johnqpublic",
+      "location" => "Anytown, USA",
+      "time_zone" => "Chicago",
+      "lang" => "en",
+      "created_at" => nil,
+      "updated_at" => nil
+    }
+  end
+
+  it "returns user with auth details set" do
+    expect(user).to receive(:save) { true }
+    expect(subject.serializable_hash).to eq(user_details)
+  end
+end
