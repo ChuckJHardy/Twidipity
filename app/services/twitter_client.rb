79856882489325f5c19e24c542386d75483c6a49
Charles J Hardy <chuckjhardy@gmail.com>
2014-11-20 17:27:45 +0000

    Add Twitter Twidipity Access Token

--- app/services/twitter_client.rb
+++ app/services/twitter_client.rb
@@ -1,6 +1,7 @@
 class TwitterClient
-  def initialize(user_id:)
+  def initialize(user_id:, user_access: true)
     @user_id = user_id
+    @user_access = user_access
   end
 
   def self.call(*args)
@@ -9,16 +10,32 @@ class TwitterClient
 
   def call
     Twitter::REST::Client.new do |config|
-      config.consumer_key        = Rails.application.secrets.twitter_api_key
-      config.consumer_secret     = Rails.application.secrets.twitter_api_secret
-      config.access_token        = user.token
-      config.access_token_secret = user.secret
+      config.consumer_key        = secrets.twitter_app_key
+      config.consumer_secret     = secrets.twitter_app_secret
+      config.access_token        = access_token
+      config.access_token_secret = access_token_secret
     end
   end
+
+  protected
+
+  attr_reader :user_access
 
   private
 
   def user
     @user ||= User.find @user_id
+  end
+
+  def secrets
+    Rails.application.secrets
+  end
+
+  def access_token
+    user_access ? user.token : secrets.twitter_access_token
+  end
+
+  def access_token_secret
+    user_access ? user.secret : secrets.twitter_access_token_secret
   end
 end
