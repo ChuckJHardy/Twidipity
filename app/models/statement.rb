030702e740206c462087d4aa9f421c22b233e565
Charles J Hardy <chuckjhardy@gmail.com>
2014-12-13 11:37:34 +0000

    Add Scope to find Statements that have ended

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -3,4 +3,6 @@ class Statement < ActiveRecord::Base
 
   belongs_to :user
   has_and_belongs_to_many :suggestions, dependent: :destroy
+
+  scope :ended, ->(date) { where('ending_at < ?', date || DateTime.now) }
 end
--- spec/models/statement_spec.rb
+++ spec/models/statement_spec.rb
@@ -13,4 +13,27 @@ RSpec.describe Statement, type: :model do
       expect(subject).to eq(expected_statuses)
     end
   end
+
+  describe ".ended" do
+    subject(:scope) { described_class.ended(date) }
+
+    let!(:statement) do
+      create(:statement_with_suggestions, suggestions_ending_at: ending_at)
+    end
+
+    let!(:ending_at) { DateTime.now + 2.days }
+    let(:date) { DateTime.now + 3.days }
+
+    it 'returns expected statements' do
+      expect(scope.size).to eq(1)
+    end
+
+    context 'when no statements have ended' do
+      let(:date) { DateTime.now + 1.days }
+
+      it 'returns expected statements' do
+        expect(scope.size).to eq(0)
+      end
+    end
+  end
 end
