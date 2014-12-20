335ffd2d1e45ed71989c1f3d23da49a96304c23a
Charles J Hardy <chuckjhardy@gmail.com>
2014-12-20 17:16:09 +0000

    Remove Capistrano Binstrubs

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,11 +24,13 @@ ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 set :use_sudo, false
 
+set :bundle_binstubs, nil
+
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
